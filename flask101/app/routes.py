a86055b63198a0d9fd48efd37f896a1ea753f6f3
1stby <love970120@gmail.com>
2024-03-06 15:04:46 +0800

    add login

--- flask101/app/routes.py
+++ flask101/app/routes.py
@@ -1,11 +1,13 @@
 from datetime import datetime, timezone
 from urllib.parse import urlsplit
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, session
 from flask_login import login_user, logout_user, current_user, login_required
+from flask_wtf.csrf import generate_csrf
 import sqlalchemy as sa
 from app import app, db
 from app.forms import LoginForm,RegistrationForm, EditProfileForm
 from app.models import User
+from werkzeug.security import check_password_hash
 
 
 @app.route('/')
@@ -25,23 +27,24 @@ def index():
     ]
     return render_template('index.html', title='首頁',  posts=posts)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = db.session.scalar(
-            sa.select(User).where(User.username == form.username.data))
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or urlsplit(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = db.session.query(User).filter_by(username=username).first()
+        if user and check_password_hash(user.password_hash, password):
+            login_user(user)
+            session['logged_in'] = True
+            session['username'] = username
+            return redirect(url_for('index'))
+        else:
+            return 'Invalid username/password'
+    return render_template('login.html', title='Sign In', csrf_token=generate_csrf)
+        
 
 
 @app.route('/logout')
